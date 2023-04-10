7b075ee908b169683c31e4c471c55c708f51c202
gobario <dlavova@mail.ru>
2023-04-10 16:23:29 +0500

    homework done

--- lesson2/src/main/java/Main.java
+++ lesson2/src/main/java/Main.java
@@ -1,2 +1,34 @@
 public class Main {
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        try {
+            int result = sumArray(array);
+            System.out.println("Сумма элементов массива: " + result);
+        } catch (MyArraySizeException e){
+            System.out.println(e.getMessage());
+        } catch (MyArrayDataException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return sum;
+    }
 }
---
+++ lesson2/src/main/java/MyArraySizeException.java
@@ -0,0 +1,5 @@
+public class MyArraySizeException  extends Exception{
+    public MyArraySizeException() {
+        super("Размер массива должен быть 4х4");
+    }
+}
