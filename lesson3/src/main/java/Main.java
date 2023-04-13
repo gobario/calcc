2d589b692b033625f9243272ecf2dd922e026b37
gobario <dlavova@mail.ru>
2023-04-13 18:45:17 +0500

    lesson3 done

--- lesson3/src/main/java/Main.java
+++ lesson3/src/main/java/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        Box<Apple> boxOfApples = new Box<>();
+        boxOfApples.add(new Apple());
+        boxOfApples.add(new Apple());
+        boxOfApples.add(new Apple());
+        Box<Orange> boxOfOranges = new Box<>();
+        boxOfOranges.add(new Orange());
+        boxOfOranges.add(new Orange());
+        System.out.println("Вес коробки с яблоками: " + boxOfApples.getWeight());
+        System.out.println("Вес коробки с апельсинами: " + boxOfOranges.getWeight());
+        System.out.println("Коробки равны по весу: " + boxOfApples.compare(boxOfOranges));
+        Box<Apple> anotherBoxOfApples = new Box<>();
+        boxOfApples.moveTo(anotherBoxOfApples);
+        System.out.println("Вес первой коробки после пересыпания: " + boxOfApples.getWeight());
+        System.out.println("Вес второй коробки после пересыпания: " + anotherBoxOfApples.getWeight());
+    }
 }
