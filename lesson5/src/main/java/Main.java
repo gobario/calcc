778df2b9921d1bdfcb57eaa5669df679df3b09f2
gobario <dlavova@mail.ru>
2023-04-26 19:25:25 +0500

    lesson 5 done

--- lesson5/src/main/java/Main.java
+++ lesson5/src/main/java/Main.java
@@ -1,2 +1,29 @@
+import java.io.IOException;
 public class Main {
+    public static void main(String[] args) {
+        String[] header = {"Value 1", "Value 2", "Value 3"};
+        int[][] data = {{100, 200, 123}, {300, 400, 500}};
+        AppData appData = new AppData(header, data);
+
+        try {
+            appData.save("data.csv");
+            appData.load("data.csv");
+
+            System.out.println("Header:");
+            for (String s : appData.header) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+
+            System.out.println("Data:");
+            for (int[] row : appData.data) {
+                for (int value : row) {
+                    System.out.print(value + " ");
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
